cdbec8e4a5e49fe69eea9f885865a5bccfc493e0
Giovanni Almeida <69661645+GiovanniBot@users.noreply.github.com>
2023-01-15 04:16:52 -0300

    model & routes segregation & routes

--- api.js
+++ api.js
@@ -1,39 +1,41 @@
 const express = require("express");
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// API Routes.
+const personRoutes = require('./routes/personRoutes');
+ 
+app.use('/person', personRoutes);
+
 app.get("/", (req, res, next) => {
   res.json({
     message: "iai",
   });
 });
 
-// Future-proof snippet for Mongoose 7.
-mongoose.set('strictQuery', false);
+// * Future-proof snippet for Mongoose 7.
+mongoose.set("strictQuery", false);
 
-// DB Connection.
 const dbUsername = encodeURIComponent(process.env.DB_USER);
 const dbPassword = encodeURIComponent(process.env.DB_PW);
-if ((dbUsername && dbPassword).length < 1) return console.log('Enviroment variables wasn\'t defined.');
+if ((dbUsername && dbPassword).length < 1)
+  return console.log("Enviroment variables wasn't defined.");
 const dbQueryStr = `mongodb+srv://${dbUsername}:${dbPassword}@api-rest.nvbu4oa.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose
-    .connect(dbQueryStr)
-    .then(() => {
-        try {
-            app.listen(port, () => console.log(`MongoDB Atlas connected!`));
-        } catch (err) {
-            console.log(err);
-        }
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .connect(dbQueryStr)
+  .then(() => {
+    try {
+      app.listen(port, () => console.log(`MongoDB Atlas connected!`));
+    } catch (err) {
+      console.log(err);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ routes/personRoutes.js
@@ -0,0 +1,98 @@
+const router = require("express").Router();
+
+const Person = require("../models/Person");
+
+router.post("/", async (req, res) => {
+  const { name, salary, approved } = req.body;
+
+  if (!name || !salary || (typeof approved !== "boolean" && approved.length < 0)) {
+    res
+      .status(422)
+      .json({ message: "Name, salary, and approved fields are required" });
+    return;
+  }
+
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+
+  try {
+    const result = await Person.create(person);
+    res
+      .status(201)
+      .json({ message: "Record created successfully", data: result });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Error: Couldn't create the record. Internal Server Error",
+    });
+    return;
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const doc = await Person.find();
+    res.status(200).json(doc);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Error: Couldn't retrieve the records. Internal Server Error",
+    });
+    return;
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const doc = await Person.findById(id);
+    if (!doc) {
+      es.status(404).json({ error: "Error: Couldn't find the record." });
+      return;
+    }
+    res.status(200).json(doc);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Error: Couldn't retrieve the record. Internal Server Error",
+    });
+    return;
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, salary, approved } = req.body;
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+
+  try {
+    const updatedDoc = await Person.updateOne({ _id: id }, person);
+
+    if (updatedDoc.matchedCount === 0 || !updatedDoc) {
+      res.status(404).json({ error: "Error: Couldn't find the record." });
+      return;
+    }
+    
+    res
+      .status(200)
+      .json({ message: "The record has been updated successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Error: Couldn't update the record. Internal Server Error",
+    });
+    return;
+  }
+});
+
+// TODO: Delete route.
+
+module.exports = router;
