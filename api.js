b0003181100f5aff78ee84116d72fadef0fc5140
Giovanni Almeida <69661645+GiovanniBot@users.noreply.github.com>
2023-01-17 00:29:34 -0300

    Add: Root endpoint returns util information

--- api.js
+++ api.js
@@ -12,18 +12,33 @@ const personRoutes = require("./routes/personRoutes");
 
 app.use("/person", personRoutes);
 
-app.use((req, res) => {
-  res.status(200).json({ message: "Welcome to my RESTful API project. You can send a GET request to the endpoint /person/ to see the object pattern and use all the functionalities of this API. Try it yourself and have fun!" });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    version: "1.0",
+    message: "Welcome to my RESTful API project. You can send a GET request to the endpoint /person/ to see the object pattern and use all the functionalities of this API. Try it yourself and have fun!",
+    endpoints: [
+      {
+        path: "/person",
+        methods: ["GET", "POST", "PATCH", "DELETE"]
+      },
+      {
+        path: "/",
+        methods: "GET"
+      }
+    ]
+  });
 });
 
-app.use("/*", (req, res) => {
+app.use((req, res) => {
   req.status(404).render("404");
 });
 
 connectToDb()
   .then(() => {
     try {
-      app.listen(port, () => console.log(`Connection with Atlas MongoDB established`));
+      app.listen(port, () =>
+        console.log(`Connection with Atlas MongoDB established`)
+      );
     } catch (err) {
       console.log(err);
     }
